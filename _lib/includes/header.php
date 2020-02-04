a2f83d2e4d74d163cc1b5a60439f3afbb423ff36
Nathan Hiemstra <nathanhiemstra@gmail.com>
2020-02-04 15:23:42 -0500

    Initial commit with Blaze, Vue, and a skeleton home page.

---
+++ _lib/includes/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> -->
+ <!--  <link rel="stylesheet" href="/_lib/css/blaze.min.css"> -->
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+  <link rel="stylesheet" href="/_lib/css/themes/blaze.nh.css">
+<!--   <link rel="stylesheet" href="/_lib/css/blaze.animations.min.css">
+  <link rel="stylesheet" href="/_lib/css/blaze.colors.min.css"> -->
+
+  <title>NH</title>
+  <style>
+    body {
+      padding: 0 0 50px 0;
+    }
+
+    .o-demo-container {
+      position: relative;
+      height: 550px;
+      margin-top: 1em;
+      overflow: hidden;
+    }
+
+    .o-demo-container--modal {
+      background-image: url(//placehold.it/1024x963/42A5F5/FFF);
+      background-size: cover;
+      background-position: center;
+    }
+
+    .o-grid--demo.o-grid {
+      border: 2px solid #ff69b4;
+    }
+
+    .o-grid--demo .o-grid__cell {
+      border: 2px solid #00bfff;
+      background-color: #b2f5b5;
+    }
+  </style>
+</head>
+
+<body class="c-text">
