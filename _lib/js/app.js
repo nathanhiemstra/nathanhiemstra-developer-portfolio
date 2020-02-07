e74f5e955e9472d46f96d4a644e3e7f8e83bc96d
Nathan Hiemstra <nathanhiemstra@gmail.com>
2020-02-07 10:38:41 -0500

    Adding text, images, and general formatting

---
+++ _lib/js/app.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+      skills: [
+        { text: 'HTML' },
+        { text: 'CSS3' },
+        { text: 'SASS' },
+        { text: 'Responsive' },
+        { text: 'Git' },
+        { text: 'JavaScript' },
+        { text: 'jQuery' },
+        { text: 'Gulp' },
+        { text: 'Jekyll' },
+        { text: 'Twig' },
+        { text: 'Pattern Libraries' },
+        { text: 'PHP' },
+        { text: 'Wordpress' },
+        { text: 'Squarespace' },
+        { text: 'Bootstrap' },
+        { text: 'Salesforce' },
+        { text: 'Sitecore' },
+        { text: 'Magnolia' },
+        { text: 'Deploybot' }
+      ],
+      
+  }
+})
+
+
+
